279b9fdc1a56d8858ef3d67254e6c434714019a9
BitTim <tim.anhalt.2004@protonmail.com>
2021-02-26 13:25:27 +0100

    Started enemy AI and edited cliffs

--- Assets/Scripts/Entities/EnemyBase.cs
+++ Assets/Scripts/Entities/EnemyBase.cs
@@ -5,6 +5,19 @@ using UnityEngine;
 public class EnemyBase : MonoBehaviour
 {
     // --------------------------------
+    //  States
+    // --------------------------------
+
+    private enum State
+	{
+        IDLE,
+        MOVE_WANDER,
+        MOVE_TARGET,
+        ATTACK,
+        DEAD
+	}
+
+    // --------------------------------
     //  Parameters
     // --------------------------------
 
@@ -15,26 +28,107 @@ public class EnemyBase : MonoBehaviour
     // --------------------------------
 
     private float health;
+    private State state = State.IDLE;
 
     // ================================
     //  Functions
     // ================================
 
-    void Start()
+    private void Start()
     {
         health = maxHealth;
     }
+
+	private void Update()
+	{
+		switch(state)
+		{
+            case State.IDLE:
+                idleState();
+                break;
+
+            case State.MOVE:
+                moveState();
+                break;
+
+            case State.ATTACK:
+                attackState();
+                break;
+
+            case State.DEAD:
+                deadState();
+                break;
+        }
+	}
+
+    // ================================
+    //  State Handler
+    // ================================
+
+    private void idleState()
+	{
+        idle();
+
+
+	}
+
+    private void moveState()
+    {
+        move();
+    }
+
+    private void attackState()
+    {
+        attack();
+    }
+
+    private void deadState()
+    {
+        dead();
+    }
+
+    // ================================
+    //  States
+    // ================================
+
+    private void idle()
+    {
+        
+    }
+
+    private void move()
+    {
+        
+    }
+
+    private void attack()
+    {
+        
+    }
+
+    private void dead()
+    {
+        
+    }
+
+    // ================================
+    //  Damage
+    // ================================
+
+    void die()
+    {
+        Collider2D[] colliders = GetComponents<Collider2D>();
+        foreach (Collider2D collider in colliders) collider.enabled = false;
+        this.enabled = false;
+    }
+
+    // ================================
+    //  Functions
+    // ================================
 
     public void OnReceiveDamage(float damage)
 	{
         health -= damage;
         if (health <= 0) die();
-	}
-
-    void die()
-	{
-        Collider2D[] colliders = GetComponents<Collider2D>();
-        foreach (Collider2D collider in colliders) collider.enabled = false;
-        this.enabled = false;
 	}
 }
