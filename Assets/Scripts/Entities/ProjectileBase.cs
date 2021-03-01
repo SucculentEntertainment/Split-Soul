c1b15561e1fa75c1587c2ab38402106a58faac3b
BitTim <tim.anhalt.2004@protonmail.com>
2021-03-02 00:40:08 +0100

    Started implementing Projectiles, just cerated basic structure

---
+++ Assets/Scripts/Entities/ProjectileBase.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBase : MonoBehaviour
+{
+    // --------------------------------
+    //  Parameters
+    // --------------------------------
+
+    public float lifespan;
+    public float baseAttack;
+
+    public Transform attackPoint;
+    public float attackRange;
+    public LayerMask attackLayers;
+
+    // --------------------------------
+    //  Internal Values
+    // --------------------------------
+
+    // ================================
+    //  Functions
+    // ================================
+
+    // TODO: Implement Projectiles
+
+    // ================================
+    //  Damage
+    // ================================    
+
+    public virtual void damage()
+    {
+        Collider2D[] hitEntities = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, attackLayers);
+        foreach(Collider2D hit in hitEntities)
+        {
+            GameEventSystem.current.GiveDamage(hit.name, baseAttack);
+        }
+    }
+
+    // ================================
+    //  Gizmos
+    // ================================
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(255, 0, 0);
+        if (attackPoint != null) Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
