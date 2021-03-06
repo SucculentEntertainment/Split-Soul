340bb29c68ec11b8ea13731f7e198a482ee206bb
BitTim <tim.anhalt.2004@protonmail.com>
2021-03-06 15:27:49 +0100

    Finished Projectiles

--- Assets/Scripts/Entities/Projectiles/ProjectileBase.cs
+++ Assets/Scripts/Entities/Projectiles/ProjectileBase.cs
@@ -12,20 +12,21 @@ public class ProjectileBase : MonoBehaviour
 	{
         CREATE,
         TRAVEL,
-        DESTROY
+        DESTROY,
+        UNINIT
 	}
 
-    protected string[] animationTrigger = { "Create", "Travel", "Destroy" };
+    protected string[] animationTrigger = { "Create", "Travel", "Destroy", "" };
     
     // --------------------------------
     //  Parameters
     // --------------------------------
 
-    public string projectileName;
     public string element;
 
     public float lifespan;
     public float baseAttack;
+    public float speed;
 
     public bool canHitMultiple = false;
 
@@ -37,6 +38,10 @@ public class ProjectileBase : MonoBehaviour
     //  Internal Values
     // --------------------------------
 
+    protected Rigidbody2D rb;
+    protected Vector2 dir;
+    protected string creator;
+    
     protected State state = State.CREATE; 
     protected float lifeTimer = 0;
 
@@ -44,24 +49,36 @@ public class ProjectileBase : MonoBehaviour
     protected bool isDestroyed = false;
 
     protected ProjectileData data;
+    protected bool impulseGiven = false;
 
     // ================================
     //  Functions
     // ================================
 
+    public void init(Vector2 dir, string creator)
+    {
+        this.dir = dir;
+        this.creator = creator;
+
+        setState(State.CREATE);
+    }
+
     protected void Start() {
-        data = new ProjectileData(projectileName, element, baseAttack);
+        data = new ProjectileData(gameObject.name, element, baseAttack);
+
+        rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+
+        init(new Vector2(1, 0), "Player");
     }
 
     protected void Update() {
         lifeTimer += Time.deltaTime;
 
-        if(lifeTimer >= lifespan)
-        {
-            isDestroyed = true;
-            setState(State.DESTROY);
-        }
+        if(lifeTimer >= lifespan) destruct();
+
+        animator.SetFloat("DirX", dir.x);
+        animator.SetFloat("DirY", dir.y);
     }
 
     protected void FixedUpdate()
@@ -88,22 +105,32 @@ public class ProjectileBase : MonoBehaviour
 
     protected void createState()
     {
+        if(isDestroyed) return;
         StartCoroutine(Wait(animator.GetCurrentAnimatorStateInfo(0).length, true, State.TRAVEL));
     }
 
     protected void travelState()
     {
-        if(hit()) setState(State.DESTROY);
+        if(isDestroyed) return;
+
+        if(!impulseGiven)
+        {
+            impulseGiven = true;
+            rb.AddForce(speed * dir, ForceMode2D.Impulse);
+        }
+
+        if(hit()) destruct();
     }
 
     protected void destroyState()
     {
-        StartCoroutine(Wait(animator.GetCurrentAnimatorStateInfo(0).length));
         Destroy(gameObject);
     }
 
     protected void setState(State state, bool changeAnim = true)
 	{
+        if(isDestroyed && state != State.DESTROY) return;
+
         if(changeAnim) animator.SetTrigger(animationTrigger[(int) state]);
         this.state = state;
 	}
@@ -117,11 +144,38 @@ public class ProjectileBase : MonoBehaviour
         Collider2D[] hitEntities = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, attackLayers);
         foreach(Collider2D hit in hitEntities)
         {
+            if(hit.name == creator) continue;
+
             GameEventSystem.current.ProjectileHit(hit.name, data);
             if(!canHitMultiple) return true;
         }
 
         return false;
+    }
+
+    private void destruct()
+    {
+        if(isDestroyed) return;
+        isDestroyed = true;
+
+        rb.drag = 5f;
+
+        animator.SetTrigger(animationTrigger[(int) State.DESTROY]);
+        StartCoroutine(Wait(animator.GetCurrentAnimatorStateInfo(0).length, true, State.DESTROY, false));
+    }
+
+    // ================================
+    //  Events
+    // ================================
+
+    private void OnDimensionEnable(string dimension)
+    {
+
+    }
+
+    private void OnDimensionDisable(string dimension)
+    {
+
     }
 
     // ================================
