c7931f1dcb989632ad6c60f9f26b43f52c7199aa
BitTim <tim.anhalt.2004@protonmail.com>
2021-05-08 16:31:14 +0200

    Added BackgroundBlur when Menu opens

--- Assets/Scripts/GUI/MainBook.cs
+++ Assets/Scripts/GUI/MainBook.cs
@@ -4,33 +4,8 @@ using UnityEngine;
 
 public class MainBook : MonoBehaviour
 {
-    public void OnResumeButton()
+	public void throwUIActionEvent(string action)
 	{
-		GameEventSystem.current.UIAction("MainResume");
-	}
-
-	public void OnInventoryButton()
-	{
-		GameEventSystem.current.UIAction("MainInventory");
-	}
-
-	public void OnStatsButton()
-	{
-		GameEventSystem.current.UIAction("MainStats");
-	}
-
-	public void OnSavesButton()
-	{
-		GameEventSystem.current.UIAction("MainSaves");
-	}
-
-	public void OnSettingsButton()
-	{
-		GameEventSystem.current.UIAction("MainSettings");
-	}
-
-	public void OnExitButton()
-	{
-		GameEventSystem.current.UIAction("MainExit");
+		GameEventSystem.current.UIAction(action);
 	}
 }
--- Assets/Scripts/GUI/UIController.cs
+++ Assets/Scripts/GUI/UIController.cs
@@ -13,51 +13,81 @@ public class Menu
 
 public class UIController : MonoBehaviour
 {
+	[Header("Effects")]
+	public UnityEngine.Rendering.VolumeProfile postProcessingProfile;
+
+	public float deafultDoF = 4;
+	public float blurDoF = 0.1f;
+	public float blurAnimDuration = 0.5f;
+
+	[Header("UI Controls")]
     public bool isTitleScreen;
+
 	public List<Menu> menues;
 	private string openedMenu = "";
 
+	UnityEngine.Rendering.Universal.DepthOfField dof;
+
+	private void Start() { if(!postProcessingProfile.TryGet(out dof)) throw new System.NullReferenceException(nameof(dof)); }
+
+	// ================================
+	//  Action Matching
+	// ================================
+
     private void OnUIAction(string action)
     {
-		switch(action)
+		if(action == "ESC")
 		{
-			case "ESC":
-				if(openedMenu == "") openMenu("MainBook");
-				else closeMenu(openedMenu);
-				break;
-
-			case "MainResume":
-				closeMenu("MainBook");
-				break;
-
-			case "MainInventory":
-				break;
-
-			case "MainStats":
-				break;
-
-			case "MainSaves":
-				break;
-
-			case "MainSettings":
-				break;
-
-			case "MainExit":
-				break;
+			if(openedMenu == "") openMenu("MainBook");
+			else closeMenu(openedMenu);
 		}
+
+		if(action == "MainReturn") { closeMenu("MainBook"); }
+		if(action == "MainInventory") { }
+		if(action == "MainStats") { }
+		if(action == "MainSaves") { }
+		if(action == "MainSettings") { }
+		if(action == "MainExit") { }
     }
+
+	// ================================
+	//  Menu control
+	// ================================
 
 	private void openMenu(string menuID)
 	{
+		StartCoroutine(blurAnimation(false, blurAnimDuration));
+
 		openedMenu = menuID;
 		menues.Find(x => x.menuID == menuID).menuContainer.SetActive(true);
 	}
 
 	private void closeMenu(string menuID)
 	{
-		if(openedMenu != menuID) return;
+		StartCoroutine(blurAnimation(true, blurAnimDuration));
 
 		menues.Find(x => x.menuID == menuID).menuContainer.SetActive(false);
 		openedMenu = "";
+
+		dof.focusDistance.Override(deafultDoF);
+	}
+
+	IEnumerator blurAnimation(bool invert, float duration)
+	{
+		float timePassed = 0;
+
+		while(true)
+		{
+			timePassed += Time.deltaTime;
+			if(timePassed >= duration) break;
+
+			float val = timePassed / duration;
+			if(invert) val = 1 - val;
+
+			dof.focusDistance.Override(Mathf.Lerp(deafultDoF, blurDoF, val));
+			yield return null;
+		}
+
+		yield return null;
 	}
 }
